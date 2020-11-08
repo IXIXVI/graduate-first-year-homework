8460b6d76bc9c80bb53cc5405b7455ac010664d0
IXIXVI <35400164+IXIXVI@users.noreply.github.com>
2020-11-08 16:57:53 +0800

    进度缓慢啊呜呜

---
+++ homework/tree.py
@@ -0,0 +1,2 @@
+a=((((sum(sum(x[:,i,0])))/c1)*log((sum(sum(x[:,i,0])))//c1))+((sum(sum(x[:,i,1])))/c1)*log((sum(sum(x[:,i,1])))/c1))
+a=((((sum(sum(x[:,:,0])))/(c1))*(log((sum(sum(x[:,:,0])))/(c1))))+(((sum(sum(x[:,:,1])))/(c1))*(log((sum(sum(x[:,:,1])))/(c1)))))
